2d02275c25da8317490ca5aeba2ec50e010bd5d1
aasjunior <adeldivo@hotmail.com>
2023-12-14 20:37:47 -0300

    implemented flask-assets to compress and minify css

--- app/app.py
+++ app/app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_assets import Environment, Bundle
 from controller import routes
 
-app = Flask(__name__, template_folder="view/templates")
+app = Flask(__name__, template_folder='view/templates', static_folder='view/static')
+assets = Environment(app)
+
+css = Bundle('css/blades/config.css', 'css/blades/form.css', filters='cssmin', output='css/styles.min.css')
+assets.register('cssmin', css)
 
 routes.init(app)
 
--- app/controller/routes.py
+++ app/controller/routes.py
@@ -4,3 +4,7 @@ def init(app):
     @app.route("/")
     def index():
         return render_template("index.html", title="Pagina Inicial")
+    
+    @app.route("/pacientes/cadastro")
+    def cadastro_paciente():
+        return render_template("pages/pacientes/cadastro_paciente.html", title="Cadastro de Paciente")
